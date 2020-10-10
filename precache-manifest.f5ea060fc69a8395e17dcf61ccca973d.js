7f4bda607c15fe3c8e30ef5f220876bc34cc64ec
snsayed06 <snsayed06@gmail.com>
2020-10-09 21:12:57 -0700

    Updates

---
+++ precache-manifest.f5ea060fc69a8395e17dcf61ccca973d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe05bc3e76948d4551c2431bd88cc201",
+    "url": "/Pagination-using-React-Hooks/index.html"
+  },
+  {
+    "revision": "006a2459851b485855ea",
+    "url": "/Pagination-using-React-Hooks/static/css/main.7591def3.chunk.css"
+  },
+  {
+    "revision": "9024af566f05aa3a301b",
+    "url": "/Pagination-using-React-Hooks/static/js/2.7ee89ea2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pagination-using-React-Hooks/static/js/2.7ee89ea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006a2459851b485855ea",
+    "url": "/Pagination-using-React-Hooks/static/js/main.757dcc63.chunk.js"
+  },
+  {
+    "revision": "dc1d20362408ba876eb3",
+    "url": "/Pagination-using-React-Hooks/static/js/runtime-main.a9b32b25.js"
+  }
+]);
